54303e58e9d7caa571bd1c19a65c605a08e669c9
Jxouille <steinhart.axel@gmail.com>
2026-09-23 11:58:58 +0200

    exercice2 finie

--- exercise2_balanced_symbols.py
+++ exercise2_balanced_symbols.py
@@ -2,7 +2,17 @@ def is_balanced(text):
 
     stack = []
 
-    matching_symbols = {')': '(',
-                        '}': '{',
-                        ']': '['}
-    
+    matching_symbols = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+    for char in text:
+        if char in matching_symbols.values():
+            stack.append(char)
+        elif char in matching_symbols.keys():
+            if not stack or stack.pop() != matching_symbols[char] or stack != []:
+                return False
+            else:
+                return True
